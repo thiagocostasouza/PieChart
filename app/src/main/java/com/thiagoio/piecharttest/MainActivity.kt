f82c847ee146ba2038c4b8af0d4af8121aabe704
thiago <thiago.souza@invillia.com>
2021-06-19 11:44:48 -0300

    add percent formatter

--- app/src/main/java/com/thiagoio/piecharttest/MainActivity.kt
+++ app/src/main/java/com/thiagoio/piecharttest/MainActivity.kt
@@ -3,10 +3,15 @@ package com.thiagoio.piecharttest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.IValueFormatter
+import com.github.mikephil.charting.formatter.PercentFormatter
+import com.github.mikephil.charting.formatter.ValueFormatter
 import com.github.mikephil.charting.utils.ColorTemplate
+import com.github.mikephil.charting.utils.ViewPortHandler
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,12 +26,17 @@ class MainActivity : AppCompatActivity() {
 
    private fun setPieChart() {
       //Setup pie Entries
+
+
        val pieEntries = arrayListOf<PieEntry>()
-       pieEntries.add(PieEntry(30.0f, "Android"))
+       pieEntries.add(PieEntry(30.0f , "Android" ))
        pieEntries.add(PieEntry(40.0f, "Flutter"))
        pieEntries.add(PieEntry(35.0f, "Java"))
        pieEntries.add(PieEntry(50.0f, "Fullstack"))
        pieEntries.add(PieEntry(10.0f, "Outras Trilhas"))
+
+
+
 
 
        //Setup Pie Chart Animation
@@ -41,22 +51,34 @@ class MainActivity : AppCompatActivity() {
            colors.add(color)
        }
 
+       //setup value format
+
+
        //Setup Piechart Entries Colers
        val pieDataSet = PieDataSet(pieEntries, "Insiders")
        pieDataSet.valueTextSize = 10f
        pieDataSet.valueTextColor = R.color.blue
        pieDataSet.colors = colors
-       pieChart.setUsePercentValues(true)
-       // Set Pie Data Set in PieData
-       val pieData = PieData(pieDataSet)
+
 
 
 
        //Now setup text in PieChart Center
        pieChart.centerText =  "Insiders"
+       pieChart.setUsePercentValues(true)
        pieChart.setCenterTextColor(resources.getColor(R.color.black))
        pieChart.setCenterTextSize(15f)
        pieChart.setEntryLabelTextSize(8f)
+
+       // Set Pie Data Set in PieData
+
+       val pieData = PieData(pieDataSet)
+
+       pieData.setValueFormatter(PercentFormatter(pieChart))
+
+
+
+
 
 
        // Now Lets Hide the PieChar Entries Tags
@@ -66,11 +88,12 @@ class MainActivity : AppCompatActivity() {
        pieChart.description.isEnabled = false
 
 
-
-
        //This enabled the on each pieEntry
        pieData.setDrawValues(true)
-
        pieChart.data = pieData
     }
+
+
+
+
 }
